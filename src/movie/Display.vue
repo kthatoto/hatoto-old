5973f5e8eaca837bf11df1b26c3b9a36622c922e
kthatoto <kthatoto@gmail.com>
2018-06-18 01:37:21 +0900

    Added wire lines

--- src/movie/Display.vue
+++ src/movie/Display.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="display">
+    <div class="verticalLine"></div>
+    <div class="holizotalLine"></div>
     <Introduction v-if="time <= 10" :time="time"/>
   </div>
 </template>
@@ -16,5 +18,20 @@ export default {
   height: 100vh;
   min-width: 1000px;
   min-height: 600px;
+  border: 10px solid black;
+}
+.verticalLine {
+  position: absolute;
+  height: 100%;
+  width: 0;
+  left: 50%;
+  border-left: 1px solid black;
+}
+.holizotalLine {
+  position: absolute;
+  height: 0;
+  width: 100%;
+  top: 50%;
+  border-top: 1px solid black;
 }
 </style>
