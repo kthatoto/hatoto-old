0eaf311d9145df92b79b80fe1dd23ca04564e4ef
kthatoto <kthatoto@gmail.com>
2018-06-22 18:55:33 +0900

    Added profile component

--- src/movie/scenes/about/About.vue
+++ src/movie/scenes/about/About.vue
@@ -1,9 +1,13 @@
 <template>
   <div class="about">
+    <Profile :time="time" :offset="offset"/>
   </div>
 </template>
 <script>
+import Profile from '@/movie/scenes/about/Profile'
+
 export default {
+  components: { Profile },
   props: ['time', 'offset']
 }
 </script>
