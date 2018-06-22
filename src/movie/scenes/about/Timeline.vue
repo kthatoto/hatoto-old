be40e2b40ea33973625194c82773bbbccbbdfac2
kthatoto <kthatoto@gmail.com>
2018-06-22 20:52:18 +0900

    Added lines data

--- src/movie/scenes/about/Timeline.vue
+++ src/movie/scenes/about/Timeline.vue
@@ -4,10 +4,41 @@
       <span>Timeline</span>
       <span class="timeline__headerUnderline"></span>
     </h2>
+    <div class="timeline__content">
+      <div class="timeline__yearLabel">
+        <span v-for="label in yearLabels"
+          class="timeline__yearLabelItem"
+          :key="label.year"
+          :style="{ left: `calc( ${label.position} - 20px )` }">{{ label.year }}</span>
+      </div>
+      <div class="timeline__baseLine"></div>
+      <div class="timeline__lines">
+        <div v-for="line in lines"
+          class="timeline__line"
+          :style="line.style"
+          :key="line.date">{{ line.date }} {{ line.title }}</div>
+      </div>
+    </div>
   </div>
 </template>
 <script>
+import linesData from './linesData'
 export default {
+  data () {
+    return {
+      yearLabels: [
+        { year: 1996, position: '0% + 20px' },
+        { year: 2015, position: '10%' },
+        { year: 2016, position: '20%' },
+        { year: 2017, position: '30%' },
+        { year: 2018, position: '80%' }
+      ],
+      lines: []
+    }
+  },
+  created () {
+    this.lines = linesData
+  }
 }
 </script>
 <style lang="scss" scoped>
@@ -16,12 +47,35 @@ export default {
     position: relative;
   }
   &__header {
-    margin-bottom: 10px;
+    margin-bottom: 20px;
     &Underline {
       display: block;
       width: 100%;
       border-bottom: 1px solid black;
     }
+  }
+  &__yearLabel {
+    position: relative;
+    height: 20px;
+    &Item {
+      position: absolute;
+      width: 40px;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+  &__baseLine {
+    border-bottom: 3px solid black;
+  }
+  &__lines {
+    position: relative;
+  }
+  &__line {
+    position: absolute;
+    height: 25px;
+    line-height: 25px;
+    vertical-align: middle;
+    padding: 0 5px;
   }
 }
 </style>
---
+++ src/movie/scenes/about/linesData.js
@@ -0,0 +1,58 @@
+export default [
+  {
+    date: '2015.4',
+    title: '明治大学',
+    style: {
+      left: '13%',
+      right: '0',
+      backgroundColor: '#33358a',
+      color: 'white',
+      top: '5px'
+    }
+  },
+  {
+    date: '2016.3',
+    title: 'エンジニアインターン',
+    style: {
+      left: '22%',
+      right: '0',
+      backgroundColor: '#f8cc46',
+      color: '#333',
+      top: '35px'
+    }
+  },
+  {
+    date: '2017.4',
+    title: '休学',
+    style: {
+      left: '46%',
+      right: '18%',
+      backgroundColor: '#9dcce0',
+      color: '#333',
+      top: '65px',
+      fontWeight: 'bold'
+    }
+  },
+  {
+    date: '2017.7',
+    title: 'エンジニアインターン',
+    style: {
+      left: '59%',
+      right: '0',
+      backgroundColor: '#61967d',
+      color: 'white',
+      top: '95px'
+    }
+  },
+  {
+    date: '2017.9',
+    title: 'エンジニアインターン',
+    style: {
+      left: '68%',
+      right: '0',
+      backgroundColor: '#b2302f',
+      color: 'white',
+      top: '125px'
+    }
+  }
+]
